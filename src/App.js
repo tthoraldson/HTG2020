0ffcf3279fc0304b19220723eb93482c1016f918
Kasia Kittel <kkittel@equalexperts.com>
2020-04-15 22:09:16 +0100

    add MaterialUI framework

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Button from '@material-ui/core/Button';
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -18,9 +22,27 @@ function App() {
         >
           Learn React
         </a>
+
+        <Hello name="Kasia" age={26 + 10} />
+
+        <Button variant="contained" color="primary">
+            Hello World
+        </Button>
+
       </header>
     </div>
   );
 }
 
-export default App;
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+export default App

a3f47c516143118279a4d65716b05442558aded4
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2020-04-16 23:22:49 -0500

    added a few files for ideas

---
+++ src/SignIn.js
@@ -0,0 +1,2 @@
+// TODO: Implement SignIn page
+// https://github.com/mui-org/material-ui/tree/master/docs/src/pages/getting-started/templates/sign-in
---
+++ src/SignUp.js
@@ -0,0 +1,2 @@
+// TODO: Implement SignUp page
+// https://github.com/mui-org/material-ui/blob/master/docs/src/pages/getting-started/templates/sign-up/SignUp.js
